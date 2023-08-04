88b33cff45ad9a437103b40701ba80ad96c76138
Pedro Lopez Mareque <pedro.lopez@clarity.ai>
2023-08-04 15:01:52 +0200

    feat: first commit

---
+++ src/components/Capitals.vue
@@ -0,0 +1,46 @@
+<script>
+import axios from 'axios'
+
+export default {
+  methods: {
+    async play() {
+      const data = (await axios.get('https://capitals.onrender.com/api/v1/capitals/play')).data
+      this.flag = data.country.flag.image
+      this.country = data.country.name
+      this.capitals = data.capitals
+    },
+    async solve(event) {
+      const capital = event.srcElement.innerText
+      const payload = {
+        country: this.country,
+        capital
+      }
+      // const data = (await axios.post('https://capitals.onrender.com/api/v1/capitals/solve', payload)).data
+      this.ok = true
+    }
+  },
+  mounted () {
+    this.play()
+  },
+  data() {
+      return {
+        flag: null,
+        country: null,
+        capitals: [],
+        ok: false
+      }
+    },
+}
+</script>
+
+<template>
+  <div>
+    <h1>{{country}}</h1>
+    <img :src="flag" style="width:300px"/>
+    <hr>
+    <button @click="solve" v-for="capital in capitals">
+      {{ capital }}
+    </button>
+    <div v-if="ok">CORRECTA!!!</div>
+  </div>
+</template>
